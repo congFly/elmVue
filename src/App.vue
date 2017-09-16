a2b9a286a6c2d83de17278bd533bad6d6a6a0e80
congFly <18829291885@163.com>
2017-09-16 16:59:17 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="app">
+    <v-header>
+      i am header!
+    </v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/rating">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <route-view></route-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import  header from './components/header/header.vue'
+
+  export  default{
+    components: {
+      'v-header': header
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .tab {
+    display: flex;
+    width: 100%;
+    line-height: 40px;
+    text-align: center;
+  }
+
+  .tab-item {
+    flex: 1;
+    text-align: center;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import goods from './components/goods/goods.vue'
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
+var app = Vue.extend(App);
+
+
+var router = new VueRouter({
+  routers: [
+    {
+      path: './goods',
+      component: goods
+    }
+  ]
+});
+
+new Vue({    //定义作用范围，挂载到id=app的div上面
   el: '#app',
-  components: { App }
-})
+  render: h => h(App),  //初始一个默认组件
+  router: router //引入路由
+});
+
