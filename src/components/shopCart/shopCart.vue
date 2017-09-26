bb55b24c952f170d65b1f4b66b6d469e25314356
fcc <fcc@cognichain.com>
2017-09-26 16:18:55 +0800

    shopCart

--- src/components/shopCart/shopCart.vue
+++ src/components/shopCart/shopCart.vue
@@ -4,16 +4,19 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight1':totalCount>0}">
+            <i class="icon-shopping_cart glyphicon glyphicon-shopping-cart"></i>
           </div>
-          <div class="num"></div>
+          <div class="num" v-show="totalCount>0">
+            {{totalCount}}
+          </div>
         </div>
-        <div class="price">123</div>
+        <div class="price" :class="{'highlight':totalCount>0}">￥{{totalPrice}}</div>
         <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
         <div class="pay">
+          ￥{{minPrice}}元起配送
         </div>
       </div>
     </div>
@@ -65,10 +68,18 @@
     background: #2b343c;
   }
 
+  .logo, .highlight1 {
+    background: rgb(0, 160, 220);
+  }
+
   .icon-shopping_cart {
     line-height: 44px;
     font-size: 24px;
     color: #80858a;
+  }
+
+  .icon-shopping_cart, .highlight {
+    color: #fff;
   }
 
   .num {
@@ -99,6 +110,10 @@
     font-weight: 700;
   }
 
+  .price .highlight {
+    color: #ffffff;
+  }
+
   .desc {
     display: inline-block;
     vertical-align: top;
@@ -108,12 +123,30 @@
     font-size: 10px;
   }
 
+  .pay {
+    height: 48px;
+    line-height: 48px;
+    text-align: center;
+    font-size: 12px;
+    font-weight: 700;
+    background: #2b333b;
+  }
+
 
 </style>
 
 <script>
   export default{
     props: {
+      selectFoods: {
+        type: Array,
+        default(){
+          return [{
+            price: 10,
+            count: 1
+          }];
+        }
+      },
       deliveryPrice: {
         type: Number,
         default: 0
@@ -122,6 +155,22 @@
         type: Number,
         default: 0
       }
+    },
+    computed: {
+      totalPrice() {
+        let total = 0;
+        this.selectFoods.forEach((food) => {
+          total += food.price * food.count;
+        });
+        return total;
+      },
+      totalCount() {
+        let count = 0;
+        this.selectFoods.forEach((food) => {
+          count += food.count;
+        });
+        return count;
+      },
     }
   };
 </script>
